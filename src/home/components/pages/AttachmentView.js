df0c45691c4ca22267c85dd958fb938df1de2de5
DAN-BIWOTT <dankibiwott254@gmail.com>
2020-02-06 15:33:23 +0300

    pdf

--- src/home/components/pages/AttachmentView.js
+++ src/home/components/pages/AttachmentView.js
@@ -5,6 +5,9 @@ import { getAttachments } from '../controllers/FireController';
 import { RadarSpinner } from 'react-epic-spinners';
 import { Col } from 'react-bootstrap';
 import '../../stylesheets/View.scss';
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import ReactPDF from '@react-pdf/renderer';
+
 
 const AttachmentView = () => {
     useEffect(
@@ -54,6 +57,7 @@ const AttachmentView = () => {
     <hr/>
     <CenterNav/>
     <div className="Row">
+        <button className="btn btn-md btn-success">Save</button>
     <div className="view col-md-12 col-sm-12">
         <div className="shadow p-3 mb-5 bg-white rounded">
         <h5 className="card-title">Attachment Locations Computer Science Year 4 Sem 1</h5>
@@ -78,5 +82,34 @@ const AttachmentView = () => {
     </Col>
     );
 }
+
+// Create styles
+const styles = StyleSheet.create({
+    page: {
+      flexDirection: 'row',
+      backgroundColor: '#E4E4E4'
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1
+    }
+  });
+   
+  // Create Document Component
+  const MyDocument = () => (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text>Section #1</Text>
+        </View>
+        <View style={styles.section}>
+          <Text>Section #2</Text>
+        </View>
+      </Page>
+    </Document>
+  );
+  
+ReactPDF.render(<MyDocument />, `${__dirname}/output.pdf`);
 
 export default AttachmentView;
