4f0fb289b74c4dba3ec794ee75dd6fcc54b4bfde
DAN-BIWOTT <dankibiwott254@gmail.com>
2020-02-07 12:28:11 +0300

    responsive size

--- src/home/components/pages/About.js
+++ src/home/components/pages/About.js
@@ -9,10 +9,9 @@ const About = () => {
                     <h2 style={{color: '#155724'}}>About Page</h2>
                     <p className="lead"></p>
                     <hr className="my-4"/>
-                    <p className="d-none d-md-block d-lg-block" id="customP">Hi, this serves as the class website.
+                    <p style={{fontSize:"20px"}} id="customP">Hi, this serves as the class website.
                     Created and maintained by Biwott.
                      Explore my content and get free designs by clicking the button below... </p>
-                     <p className="visible" id="customP">Use your laptop to view content</p>
                     <a className="btn btn-outline-success btn-lg" href="https://sghost.herokuapp.com" role="button">Learn more</a>
             </div>
         </Col>
--- src/home/components/pages/Home.js
+++ src/home/components/pages/Home.js
@@ -5,7 +5,13 @@ import { Col } from 'react-bootstrap';
 const Home = () => {
     return(
         <Col md="10" sm="10" xs="10" id="MainPage">
-        
+            <Col className="d-block d-md-none"  md="10" sm="10" xs="10">
+            <div className="col-md-10 customJumbo my-5">
+                    <p className="lead"></p>
+                    <hr className="my-4"/>
+                     <p className="visible" id="customP">Use your laptop to view content</p>
+            </div>
+            </Col>
         </Col>
     );
 }
